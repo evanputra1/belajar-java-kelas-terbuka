aad1d12a5c998e5414992fa54f42693bb55cc2b2
Evan Ananda Putra <121525443+evanputra1@users.noreply.github.com>
2023-10-26 12:31:48 +0700

    belajar tipe data

---
+++ 08-Variabel-Assignment-dan-Deklarasi/src/com/tutorial/Main.java
@@ -0,0 +1,53 @@
+// 'com.tutorial' adalah nama paket (package) di sini.
+package com.tutorial;
+
+// Ini adalah deklarasi kelas utama bernama 'Main'.
+public class Main {
+
+    // Ini adalah metode (fungsi) utama yang akan dijalankan ketika program dimulai.
+    public static void main(String[] args) {
+
+        // Ini mencetak satu baris kosong
+        System.out.println("\n");
+
+        // Ini mendeklarasikan variabel 'x' dengan tipe data integer dan memberi nilainya 10.
+        int x = 10;
+        // Ini mencetak teks "nilai x = " diikuti dengan nilai dari variabel 'x'.
+        System.out.println("nilai x = " + x);
+
+        // Ini mendeklarasikan variabel 'y' dengan tipe data float dan memberi nilainya 5.99.
+        float y = 5.99f;
+        // Ini mencetak teks "nilai float y adalah = " diikuti dengan nilai dari variabel 'y'.
+        System.out.println("nilai float y adalah = " + y);
+
+        // Ini mendeklarasikan variabel 'letter' dengan tipe data char (karakter) dan memberi nilainya 'D'.
+        char letter = 'D';
+        // Ini mencetak teks "huruf depan Dodi adalah = " diikuti dengan nilai dari variabel 'letter'.
+        System.out.println("huruf depan Dodi adalah = " + letter);
+
+        // Ini mendeklarasikan variabel 'bool' dengan tipe data boolean dan memberi nilainya 'true'.
+        boolean bool = true;
+        // Ini mencetak teks "apakah 11 > 10 = " diikuti dengan nilai dari variabel 'bool'.
+        System.out.println("apakah 11 > 10 = " + bool);
+
+        // Ini mendeklarasikan variabel 'text' dengan tipe data String (teks) dan memberi nilainya "Udin".
+        String text = "Udin";
+        // Ini mencetak teks "halo nama saya " diikuti dengan nilai dari variabel 'text'.
+        System.out.println("halo nama saya " + text);
+
+        // Ini mendeklarasikan variabel 'z' dengan tipe data double dan memberi nilainya 3.14.
+        double z = 3.14;
+        // Ini mencetak teks "nilai di pi adalah = " diikuti dengan nilai dari variabel 'z'.
+        System.out.println("nilai di pi adalah = " + z);
+
+        // Ini adalah deklarasi variabel 'a' dengan tipe data int (integer).
+        int a;
+        // Ini adalah memberikan nilai 8 ke variabel 'a'.
+        a = 8;
+        // Ini adalah perintah untuk mencetak teks "nilai a = " diikuti dengan nilai dari variabel 'a' ke konsol.
+        System.out.println("nilai a = " + a);
+
+        // Ini mencetak satu baris kosong
+        System.out.println("\n");
+    }
+}
---
+++ 09-Tipe-Data/src/com/tutorial/Main.java
@@ -0,0 +1,96 @@
+package com.tutorial;
+
+public class Main {
+    public static void main(String[] args) {
+ 
+        System.out.println("\n");
+
+        // Tipe Data di Java: 
+        // integer, byte, short, long, double, float, char, boolean 
+
+        //integer (satuan)
+        int i = 2147483647;
+        System.out.println("nilai integer i = " + i);
+        System.out.println("nilai integer i = " + (i + 1));
+        System.out.println("nilai max = " + Integer.MAX_VALUE);
+        System.out.println("nilai min = " + Integer.MIN_VALUE);
+        System.out.println("besar integer = " + Integer.BYTES + " bytes");
+        System.out.println("besar integer = " + Integer.SIZE + " bit");
+
+        System.out.println("\n");
+
+        //byte (satuan)
+        byte b = 10;
+        System.out.println("nilai byte i = " + b);
+        System.out.println("nilai byte i = " + (b + 1));
+        System.out.println("nilai max = " + Byte.MAX_VALUE);
+        System.out.println("nilai min = " + Byte.MIN_VALUE);
+        System.out.println("besar byte = " + Byte.BYTES + " bytes");
+        System.out.println("besar byte = " + Byte.SIZE + " bit");
+
+        System.out.println("\n");
+
+        //short (satuan)
+        short s = 10;
+        System.out.println("nilai short i = " + s);
+        System.out.println("nilai short i = " + (s + 1));
+        System.out.println("nilai max = " + Short.MAX_VALUE);
+        System.out.println("nilai min = " + Short.MIN_VALUE);
+        System.out.println("besar short = " + Short.BYTES + " bytes");
+        System.out.println("besar short = " + Short.SIZE + " bit");
+
+        System.out.println("\n");
+
+        //long (satuan)
+        long l = 10;
+        System.out.println("nilai long i = " + b);
+        System.out.println("nilai long i = " + (b + 1));
+        System.out.println("nilai max = " + Long.MAX_VALUE);
+        System.out.println("nilai min = " + Long.MIN_VALUE);
+        System.out.println("besar long = " + Long.BYTES + " bytes");
+        System.out.println("besar long = " + Long.SIZE + " bit");
+
+        System.out.println("\n");
+
+        //double (desimal)
+        double d = 10.5d;
+        System.out.println("nilai double i = " + d);
+        System.out.println("nilai double i = " + (d + 1));
+        System.out.println("nilai max = " + Double.MAX_VALUE);
+        System.out.println("nilai min = " + Double.MIN_VALUE);
+        System.out.println("besar double = " + Double.BYTES + " bytes");
+        System.out.println("besar double = " + Double.SIZE + " bit");
+
+        System.out.println("\n");
+
+        //float (desimal)
+        float f = 10.5f;
+        System.out.println("nilai float i = " + f);
+        System.out.println("nilai float i = " + (f + 1));
+        System.out.println("nilai max = " + Float.MAX_VALUE);
+        System.out.println("nilai min = " + Float.MIN_VALUE);
+        System.out.println("besar float = " + Float.BYTES + " bytes");
+        System.out.println("besar float = " + Float.SIZE + " bit");
+
+        System.out.println("\n");
+
+        //char (karakter)
+        char c = 'C';
+        System.out.println("nilai char c = " + c);
+        System.out.println("nilai char c = " + (c + 1));
+        System.out.println("nilai max = " + Character.MAX_VALUE);
+        System.out.println("nilai min = " + Character.MIN_VALUE);
+        System.out.println("besar char = " + Character.BYTES + " bytes");
+        System.out.println("besar char = " + Character.SIZE + " bit");
+
+        System.out.println("\n");
+
+        //boolean (nilai true atau false)
+        boolean val = true;
+        System.out.println("nilai boolean val = " + val);
+        System.out.println("nilai max = " + Boolean.TRUE);
+        System.out.println("nilai min = " + Boolean.FALSE);
+
+        System.out.println("\n");
+    }
+}
