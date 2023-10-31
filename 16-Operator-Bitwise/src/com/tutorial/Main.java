913e77983d43e7d7e12e6ee7b580954589eaebdf
Evan Ananda Putra <121525443+evanputra1@users.noreply.github.com>
2023-10-31 19:11:17 +0700

    latihan aritmatika

--- 16-Operator-Bitwise/src/com/tutorial/Main.java
+++ 16-Operator-Bitwise/src/com/tutorial/Main.java
@@ -6,15 +6,88 @@ public class Main {
         // Operator Bitwise --> operator untuk melakukan operasi pada nilai bit
 
         byte a = 3;
-        byte b;
-        String a_bits;
+        byte b, c;
+        String a_bits, b_bits, c_bits;
 
         a_bits = String.format("%8s", Integer.toBinaryString(a)).replace(' ', '0');
         System.out.printf("%s = %d \n", a_bits, a);
 
         // Operator SHIFT LEFT
-        System.out.println("===== SHIFT LEFT =====");
+        System.out.println("===== SHIFT LEFT (<<) =====");
+        a_bits = String.format("%8s", Integer.toBinaryString(a)).replace(' ', '0');
+        System.out.printf("%s = %d \n", a_bits, a);
+        b = (byte)(a << 3);
+        b_bits = String.format("%8s", Integer.toBinaryString(b)).replace(' ', '0');
+        System.out.printf("%s = %d \n", b_bits, b);
+        System.out.println("\n");
+
+        // Operator SHIFT RIGHT
+        System.out.println("===== SHIFT LEFT (>>) =====");
+        a = 24;
+        a_bits = String.format("%8s", Integer.toBinaryString(a)).replace(' ', '0');
+        System.out.printf("%s = %d \n", a_bits, a);
+
         b = (byte)(a << 1);
-        System.out.println(b);
+        b_bits = String.format("%8s", Integer.toBinaryString(b)).replace(' ', '0');
+        System.out.printf("%s = %d \n", b_bits, b);
+        System.out.println("\n");
+        
+        // Operator bitwise OR
+        System.out.println("===== BITWISE OR (|) =====");
+        a = 24;
+        a_bits = String.format("%8s", Integer.toBinaryString(a)).replace(' ', '0');
+        System.out.printf("%s = %d \n", a_bits, a);
+
+        b = 12;
+        b_bits = String.format("%8s", Integer.toBinaryString(b)).replace(' ', '0');
+        System.out.printf("%s = %d \n", b_bits, b);
+
+        System.out.println("-------------------- OR");
+        c = (byte)(a | b);
+        c_bits = String.format("%8s", Integer.toBinaryString(c)).replace(' ', '0');
+        System.out.printf("%s = %d \n", c_bits, c);
+        System.out.println("\n");
+        
+        // Operator bitwise AND
+        System.out.println("===== BITWISE AND (&) =====");
+        a = 24;
+        a_bits = String.format("%8s", Integer.toBinaryString(a)).replace(' ', '0');
+        System.out.printf("%s = %d \n", a_bits, a);
+
+        b = 12;
+        b_bits = String.format("%8s", Integer.toBinaryString(b)).replace(' ', '0');
+        System.out.printf("%s = %d \n", b_bits, b);
+
+        System.out.println("-------------------- AND");
+        c = (byte)(a & b);
+        c_bits = String.format("%8s", Integer.toBinaryString(c)).replace(' ', '0');
+        System.out.printf("%s = %d \n", c_bits, c);
+        System.out.println("\n");
+        
+        // Operator bitwise XOR
+        System.out.println("===== BITWISE XOR (^) =====");
+        a = 24;
+        a_bits = String.format("%8s", Integer.toBinaryString(a)).replace(' ', '0');
+        System.out.printf("%s = %d \n", a_bits, a);
+
+        b = 12;
+        b_bits = String.format("%8s", Integer.toBinaryString(b)).replace(' ', '0');
+        System.out.printf("%s = %d \n", b_bits, b);
+
+        System.out.println("-------------------- XOR");
+        c = (byte)(a ^ b);
+        c_bits = String.format("%8s", Integer.toBinaryString(c)).replace(' ', '0');
+        System.out.printf("%s = %d \n", c_bits, c);
+        System.out.println("\n");
+
+        // Operator bitwise Negasi / NOT
+        System.out.println("===== BITWISE NOT (~) =====");
+        a = 24;
+        b = (byte) (~a);
+        a_bits = String.format("%8s", Integer.toBinaryString(a)).replace(' ', '0');
+        System.out.printf("%s = %d \n", a_bits, a);
+        System.out.println("-------------------- NOT");
+        b_bits = String.format("%8s", Integer.toBinaryString(b)).substring(24);
+        System.out.printf("%s = %d \n", b_bits, b);
     }
 }
---
+++ 17-Latihan-Aritmatika/src/com/tutorial/Main.java
@@ -0,0 +1,10 @@
+package com.tutorial;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Membuat perhitungan menghitung luas persegi panjang
+        System.out.println("Hello World!");
+        System.out.println("\n");
+    }
+}
