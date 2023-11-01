2c5afd87d4c60c20692873729e1160969f9a212c
Evan Ananda Putra <121525443+evanputra1@users.noreply.github.com>
2023-11-01 16:29:24 +0700

    belajar if statement

--- 19-Latihan-Operator-Logika/src/com/tutorial/Main.java
+++ 19-Latihan-Operator-Logika/src/com/tutorial/Main.java
@@ -11,8 +11,22 @@ public class Main {
         // Sebuah program untuk menebak angka
         int nilaiBenar = 6;
         int nilaiTebakan;
+        boolean statusTebakan;
 
+        System.out.print("Masukan nilai tebakan anda : ");
         nilaiTebakan = inputUser.nextInt();
         System.out.println("Nilai tebakan anda : " + nilaiTebakan);
+
+        // Operasi logika
+        statusTebakan = nilaiTebakan == nilaiBenar;
+        System.out.println("Status tebakan anda : " + statusTebakan);
+        System.out.println("\n");
+
+        // Operasi aljabar boolean (AND, OR)
+        System.out.print("Masukan nilai diantara 4 dan 9");
+        nilaiTebakan = inputUser.nextInt();
+        statusTebakan = (nilaiTebakan >= 4) && (nilaiTebakan <= 9);
+        System.out.println("Status tebakan anda : " + statusTebakan);
+
     }
 }
---
+++ 20-If-Else-Statement/src/com/tutorial/Main.java
@@ -0,0 +1,21 @@
+package com.tutorial;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        // If Else Statement atau percabangan
+
+        int a = 10;
+
+        System.out.println("nilai = " + a);
+
+        // ini adalah cabangnya
+        if (a == 5) {
+            System.out.println("ini adalah jalur true");
+        } else {
+            System.out.println("ini adalah jalur false");
+        }
+        
+        System.out.println("selesai");
+    }
+}
